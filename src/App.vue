e8f987d7ea23a6a3ea2559830fcf875d824be92d
ThiagoSantosTh <thiagosantosab070@gmail.com>
2022-07-09 19:37:15 -0300

    FEATURE: Adicionando os componets fixos (Header e Footer) e os svgs/images

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,79 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link>
-    </nav>
-    <router-view></router-view>
+    <Header></Header>
+    <main id="main">
+      <router-view></router-view>
+    </main>
+    <Footer></Footer>
   </div>
 </template>
 
-<style></style>
+<script>
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+
+export default {
+  components: {
+    Header,
+    Footer,
+  },
+};
+</script>
+
+<style>
+* {
+  padding: 0px;
+  margin: 0px;
+  box-sizing: border-box;
+}
+
+ul {
+  list-style: none;
+}
+
+body {
+  font-family: "Avenir", Arial, Helvetica, sans-serif;
+  color: #345;
+  background: url(./assets/pattern.svg) repeat top;
+}
+
+a {
+  color: #345;
+  text-decoration: none;
+}
+
+img {
+  max-width: 100%;
+  display: block;
+}
+
+.btn {
+  display: block;
+  padding: 10px 30px;
+  background: #87f;
+  color: #fff;
+  border-radius: 4px;
+  text-align: center;
+  font-size: 1rem;
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.2);
+  transition: all 0.3s;
+  border: none;
+  font-family: "Avenir", Arial, Helvetica, sans-serif;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #65d;
+  transform: scale(1.1);
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+#main {
+  flex: 1;
+}
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer>
+    <p>Copyriht © 2022 Ranek | Todos os direitos reservados.</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "footerComponent",
+};
+</script>
+
+<style scoped>
+footer {
+  background: #87f;
+  padding: 25px 0;
+  text-align: center;
+  font-size: 1rem;
+  color: #fff;
+  position: relative;
+  /* top: 75px; */
+}
+</style>
